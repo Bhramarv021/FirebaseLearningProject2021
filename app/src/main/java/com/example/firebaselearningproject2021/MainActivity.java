f94663b0c5889829c008a092a647b896201dfcb7
Bhramarv021 <bhramarv021@gmail.com>
2021-05-24 03:35:52 +0530

    Remove old files and add new layout for signup

--- app/src/main/java/com/example/firebaselearningproject2021/MainActivity.java
+++ app/src/main/java/com/example/firebaselearningproject2021/MainActivity.java
@@ -1,29 +1,16 @@
 package com.example.firebaselearningproject2021;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
-import android.app.ProgressDialog;
-import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -31,38 +18,40 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
 public class MainActivity extends AppCompatActivity {
 
-    ImageView userImage;
-    Button browseImage;
-    Button uploadImage;
-    Uri filePath;
-    Bitmap bitmap;
+    ImageView userProfileImage;
+    EditText userName;
+    EditText userCourse;
+    EditText userRollNumber;
+    EditText userDuration;
+    Button browse;
+    Button signup;
+
+    Uri imagePath;
+    Bitmap imgBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_image_activity);
+        setContentView(R.layout.activity_main);
 
-        userImage = findViewById(R.id.user_image);
-        browseImage = findViewById(R.id.browse_button);
-        uploadImage = findViewById(R.id.upload_button);
+        userName = findViewById(R.id.name);
+        userRollNumber = findViewById(R.id.roll_number);
+        userDuration = findViewById(R.id.duration);
+        userCourse = findViewById(R.id.course);
+        browse = findViewById(R.id.browse_button);
+        signup = findViewById(R.id.signup_button);
 
-        browseImage.setOnClickListener(new View.OnClickListener() {
+        browse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Dexter.withContext(MainActivity.this)
-                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .withPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
                         .withListener(new PermissionListener() {
                             @Override
                             public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                                Intent intent = new Intent(Intent.ACTION_PICK);
-                                intent.setType("image/*");
-                                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-                                startActivityForResult(Intent.createChooser(intent, "Please select image"),1);
+
                             }
 
                             @Override
@@ -78,67 +67,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        uploadImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                uploadToFirebase();
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == 1 && resultCode == RESULT_OK){
-            filePath = data.getData();
-            try {
-                InputStream inputStream = getContentResolver().openInputStream(filePath);
-                bitmap = BitmapFactory.decodeStream(inputStream);
-                userImage.setImageBitmap(bitmap);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private View browseUserImage(View view){
-        return view;
-    }
-
-    private void uploadToFirebase() {
-        ProgressDialog dialog = new ProgressDialog(this);
-        dialog.setTitle("File uploader");
-        dialog.show();
-
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-
-        StorageReference refToFirebase = storage.getReference().child("YourImages");
-
-        StorageReference photoRef = refToFirebase.child(filePath.getLastPathSegment());
-
-//        StorageReference uploader = storage.getReference().child("image1");
-//        uploader.putFile(filePath)
-        photoRef.putFile(filePath)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        dialog.dismiss();
-                        Toast.makeText(MainActivity.this, "File Uploaded", Toast.LENGTH_SHORT).show();
-                        clearEditText();
-                    }
-                })
-                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onProgress( UploadTask.TaskSnapshot snapshot) {
-                        long percent = (100 * snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
-                        dialog.setMessage("Uploaded : "+percent+"%");
-                    }
-                });
-    }
-
-    private void clearEditText() {
-        filePath = null;
-        bitmap = null;
-        userImage.setImageResource(R.drawable.ic_launcher_background);
     }
 }
